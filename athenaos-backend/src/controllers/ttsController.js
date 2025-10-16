010583209e039e0197bde633516c0a56086d64b8
ThuanDanchoi <ducthuantrandc@gmail.com>
2025-10-17 09:49:14 +1100

    updated files

--- athenaos-backend/src/controllers/ttsController.js
+++ athenaos-backend/src/controllers/ttsController.js
@@ -1,44 +1,41 @@
-const { TextToSpeechClient } = require('@google-cloud/text-to-speech');
+// src/controllers/ttsController.js
+const { ElevenLabsClient } = require("elevenlabs");
 
-let textToSpeechClient;
 
-try {
-  if (process.env.GOOGLE_CREDENTIALS_BASE64) {
-    const credentialsJson = Buffer.from(process.env.GOOGLE_CREDENTIALS_BASE64, 'base64').toString('utf-8');
-    const credentials = JSON.parse(credentialsJson);
-    textToSpeechClient = new TextToSpeechClient({ credentials });
-  } else {
-    textToSpeechClient = new TextToSpeechClient();
-  }
-} catch (error) {
-  console.error('Failed to initialize TextToSpeechClient:', error);
-}
+const client = new ElevenLabsClient({
+ apiKey: process.env.ELEVENLABS_API_KEY,
+});
 
 
 exports.synthesizeSpeech = async (req, res) => {
-  if (!textToSpeechClient) {
-    return res.status(500).send('Text-to-Speech client not initialized');
-  }
+ const { text } = req.body;
 
-  const { text } = req.body;
 
-  if (!text) {
-    return res.status(400).json({ message: 'Text is required' });
-  }
+ if (!text) {
+   return res.status(400).json({ message: 'Text is required' });
+ }
 
-  try {
-    const request = {
-      input: { text: text },
-      voice: { languageCode: 'en-US', ssmlGender: 'NEUTRAL' },
-      audioConfig: { audioEncoding: 'MP3' },
-    };
 
-    const [response] = await textToSpeechClient.synthesizeSpeech(request);
-    
-    res.json({ audioContent: response.audioContent.toString('base64') });
+ try {
+   const audio = await client.generate({
+     voice: "ROMJ9yK1NAMuu1ggrjDW", 
+     text: text,
+     model_id: 'eleven_multilingual_v2',
+   });
 
-  } catch (error) {
-    console.error('ERROR synthesizing speech:', error);
-    res.status(500).send('Failed to synthesize speech');
-  }
+
+   const chunks = [];
+   for await (const chunk of audio) {
+     chunks.push(chunk);
+   }
+   const content = Buffer.concat(chunks);
+
+
+   res.json({ audioContent: content.toString('base64') });
+
+
+ } catch (error) {
+   console.error('ERROR synthesizing speech with ElevenLabs:', error);
+     res.status(500).send('Failed to synthesize speech');
+ }
 };
